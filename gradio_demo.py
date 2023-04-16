4c5e951fa560ec59576bf15777f924765fcdfdef
Bo Li <drluodian@gmail.com>
2023-04-16 08:19:37 +0000

    update gradio, exists bugs in displaying demo

---
+++ gradio_demo.py
@@ -0,0 +1,119 @@
+import matplotlib.pyplot
+import pycocotools
+from segment_anything import build_sam, SamPredictor, SamAutomaticMaskGenerator
+import numpy as np
+import gradio as gr
+from PIL import Image, ImageDraw
+from utils import iou, sort_and_deduplicate, relation_classes, MLP, show_anns, show_mask
+import torch
+import matplotlib.pyplot as plt
+import random
+
+input_size = 512
+hidden_size = 256
+num_classes = 56
+
+sam = build_sam(checkpoint="segment_anything/sam_vit_h_4b8939.pth").to('cuda')
+predictor = SamPredictor(sam)
+mask_generator = SamAutomaticMaskGenerator(sam)
+
+model = MLP(input_size, hidden_size, num_classes).to('cuda')
+model.load_state_dict(torch.load("ram/best_model.pth"))
+
+def make_gif(image, fps=5):
+    pass
+
+def draw_mask(mask, draw):
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), 153)
+    nonzero_coords = np.transpose(np.nonzero(mask))
+
+    for coord in nonzero_coords:
+        draw.point(coord[::-1], fill=color)
+
+
+# plt.imshow(image)
+# show_points(input_point, input_label, plt.gca())
+# plt.show()
+def inference(pil_image, coords, k=3):
+    width, height = pil_image.size
+    full_area = width * height
+    coords_x, coords_y = coords.split(',')
+    input_point = np.array([[int(coords_x), int(coords_y)]])
+    input_label = np.array([1])
+    image = np.array(pil_image)
+    predictor.set_image(image)
+    mask1, score1, logit1, feat1 = predictor.predict(
+        point_coords=input_point,
+        point_labels=input_label,
+        multimask_output=False,
+    )
+    
+    sam_masks = mask_generator.generate(image)
+    # remove very small items, duplicate items, and itself
+    filtered_masks = [d for d in sam_masks if iou(d['segmentation'], mask1[0]) < 0.95]
+    filtered_masks = [d for d in filtered_masks if d['area'] > 0.03 * full_area]
+    filtered_masks = sort_and_deduplicate(filtered_masks)
+    print('number of masks left:', len(filtered_masks))
+
+    # compute relations
+
+    pil_image = pil_image.convert('RGBA')
+    pil_image_list = []
+    relation_list = []
+    for i in range(len(filtered_masks)):
+        mask_image = Image.new('RGBA', pil_image.size, color=(0, 0, 0, 0))
+        mask_draw = ImageDraw.Draw(mask_image)
+        draw_mask(mask1[0], mask_draw)
+        draw_mask(filtered_masks[i]['segmentation'], mask_draw)
+
+        feat2 = torch.Tensor(filtered_masks[i]['feat']).unsqueeze(0).unsqueeze(0).to('cuda')
+        concat_input = torch.cat((feat1, feat2), dim=2)
+        output = model(concat_input)
+        topk_indices = torch.argsort(-output).flatten()
+        strings = [relation_classes[indice] for indice in topk_indices[:k]]
+        result = ', '.join(strings)
+    
+        current_pil_image = pil_image.copy()
+        # text_draw = ImageDraw.Draw(current_pil_image)
+        # text_draw.text((width // 2, height - 20), result, align="center", font=font)
+        current_pil_image.alpha_composite(mask_image)
+        pil_image_list.append(current_pil_image)
+        relation_list.append(result)
+    
+    # return [pil_image_list, relation_list]
+    yield pil_image_list, relation_list
+
+
+block = gr.Blocks()
+block = block.queue()
+with block:
+    with gr.Row():
+        with gr.Column():
+            input_image = gr.Image(source="upload", type="pil", value="images/dog.jpg")
+            img_input_coords = gr.Textbox(label="Input coords")
+            k = gr.Slider(label="TopK Relations", minimum=1, maximum=10, value=3, step=1)
+            # clear_button = gr.Button(value="Clear Click", interactive=True)
+
+            def select_handler(evt: gr.SelectData):
+                coords = evt.index
+                # draw = ImageDraw.Draw(image)
+                # draw.ellipse((coords[0] - 15, coords[1] - 15, coords[0] + 15, coords[1] + 15), fill="red")
+                return f"{coords[0]},{coords[1]}"
+
+            input_image.select(select_handler, None, img_input_coords)
+
+            run_button = gr.Button(label="Run")
+            with gr.Accordion("Advanced options", open=False):
+                box_threshold = gr.Slider(label="Box Threshold", minimum=0.0, maximum=1.0, value=0.3, step=0.001)
+                text_threshold = gr.Slider(label="Text Threshold", minimum=0.0, maximum=1.0, value=0.25, step=0.001)
+                iou_threshold = gr.Slider(label="IOU Threshold", minimum=0.0, maximum=1.0, value=0.5, step=0.001)
+                inpaint_mode = gr.Dropdown(["merge", "first"], value="merge", label="inpaint_mode")
+
+        with gr.Column():
+            image_gallery = gr.Gallery(label="Generated images", show_label=True, elem_id="gallery").style(preview=True, grid=2, object_fit="scale-down")
+            text_gallery = gr.outputs.Textbox(label="Generated relations")
+
+    run_button.click(fn=inference, inputs=[input_image, img_input_coords, k], outputs=[image_gallery, text_gallery], show_progress=True, queue=True)
+    # clear_button.click(fn=reset_image, inputs=[input_image], outputs=input_image)
+
+block.launch(debug=True, share=True)
---
+++ relate_anything.py
@@ -0,0 +1,280 @@
+import argparse
+import os
+import copy
+
+import numpy as np
+import json
+import torch
+from PIL import Image, ImageDraw, ImageFont
+
+# Grounding DINO
+import GroundingDINO.groundingdino.datasets.transforms as T
+from GroundingDINO.groundingdino.models import build_model
+from GroundingDINO.groundingdino.util.slconfig import SLConfig
+from GroundingDINO.groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
+
+# segment anything
+from segment_anything import build_sam, SamPredictor 
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from transformers import AutoProcessor, BlipForConditionalGeneration
+import torch
+import PIL
+
+import sys
+sys.path.append("../")
+import sng_parser
+from pprint import pprint
+
+# from langchain.prompts import PromptTemplate
+# from langchain.chat_models import ChatOpenAI
+# from langchain.prompts.chat import (
+#     ChatPromptTemplate,
+#     HumanMessagePromptTemplate,
+# )
+# from langchain.chains import LLMChain
+# human_message_prompt = HumanMessagePromptTemplate(
+#     prompt=PromptTemplate(
+#         template="""You are now serving as a relation detection model from given sentence. If there are multiple relations, please output multiple lines following the format. \n
+#                     [Insert sentence here] 
+#                     For example: \n
+#                     Identify the subject, relation, and object in the following sentence: President Obama and President Trump shaking hands. \n
+#                     Subject: President Obama \n
+#                     Relation: shaking hands \n
+#                     Object: President Trump \n
+#                     Please type in the sentence you want to detect the relation from: {sentence}?""",
+#         input_variables=["sentence"],
+#     )
+# )
+
+# os.environ['OPENAI_API_KEY'] = "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"
+
+def load_image(image_path):
+    # load image
+    image_pil = Image.open(image_path).convert("RGB")  # load image
+
+    transform = T.Compose(
+        [
+            T.RandomResize([800], max_size=1333),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+    image, _ = transform(image_pil, None)  # 3, h, w
+    return image_pil, image
+
+
+def load_model(model_config_path, model_checkpoint_path, device):
+    args = SLConfig.fromfile(model_config_path)
+    args.device = device
+    model = build_model(args)
+    checkpoint = torch.load(model_checkpoint_path, map_location="cpu")
+    load_res = model.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
+    print(load_res)
+    _ = model.eval()
+    return model
+
+
+def get_grounding_output(model, image, caption, box_threshold, text_threshold, with_logits=True, device="cpu"):
+    caption = caption.lower()
+    caption = caption.strip()
+    if not caption.endswith("."):
+        caption = caption + "."
+    model = model.to(device)
+    image = image.to(device)
+    with torch.no_grad():
+        outputs = model(image[None], captions=[caption])
+    logits = outputs["pred_logits"].cpu().sigmoid()[0]  # (nq, 256)
+    boxes = outputs["pred_boxes"].cpu()[0]  # (nq, 4)
+    logits.shape[0]
+
+    # filter output
+    logits_filt = logits.clone()
+    boxes_filt = boxes.clone()
+    filt_mask = logits_filt.max(dim=1)[0] > box_threshold
+    logits_filt = logits_filt[filt_mask]  # num_filt, 256
+    boxes_filt = boxes_filt[filt_mask]  # num_filt, 4
+    logits_filt.shape[0]
+
+    # get phrase
+    tokenlizer = model.tokenizer
+    tokenized = tokenlizer(caption)
+    # build pred
+    pred_phrases = []
+    for logit, box in zip(logits_filt, boxes_filt):
+        pred_phrase = get_phrases_from_posmap(logit > text_threshold, tokenized, tokenlizer)
+        if with_logits:
+            pred_phrases.append(pred_phrase + f"({str(logit.max().item())[:4]})")
+        else:
+            pred_phrases.append(pred_phrase)
+
+    return boxes_filt, pred_phrases
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_box(box, ax, label):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2)) 
+    ax.text(x0, y0, label)
+
+
+def save_mask_data(output_dir, mask_list, box_list, label_list):
+    value = 0  # 0 for background
+
+    mask_img = torch.zeros(mask_list.shape[-2:])
+    for idx, mask in enumerate(mask_list):
+        mask_img[mask.cpu().numpy()[0] == True] = value + idx + 1
+    plt.figure(figsize=(10, 10))
+    plt.imshow(mask_img.numpy())
+    plt.axis('off')
+    plt.savefig(os.path.join(output_dir, 'mask.jpg'), bbox_inches="tight", dpi=300, pad_inches=0.0)
+
+    json_data = [{
+        'value': value,
+        'label': 'background'
+    }]
+    for label, box in zip(label_list, box_list):
+        value += 1
+        name, logit = label.split('(')
+        logit = logit[:-1] # the last is ')'
+        json_data.append({
+            'value': value,
+            'label': name,
+            'logit': float(logit),
+            'box': box.numpy().tolist(),
+        })
+    with open(os.path.join(output_dir, 'mask.json'), 'w') as f:
+        json.dump(json_data, f)
+    
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser("Grounded-Segment-Anything Demo", add_help=True)
+    parser.add_argument("--config", type=str, required=True, help="path to config file")
+    parser.add_argument(
+        "--grounded_checkpoint", type=str, required=True, help="path to checkpoint file"
+    )
+    parser.add_argument(
+        "--sam_checkpoint", type=str, required=True, help="path to checkpoint file"
+    )
+    parser.add_argument("--input_image", type=str, required=True, help="path to image file")
+    # parser.add_argument("--text_prompt", type=str, required=True, help="text prompt")
+    parser.add_argument(
+        "--output_dir", "-o", type=str, default="outputs", required=True, help="output directory"
+    )
+
+    parser.add_argument("--box_threshold", type=float, default=0.3, help="box threshold")
+    parser.add_argument("--text_threshold", type=float, default=0.25, help="text threshold")
+
+    parser.add_argument("--device", type=str, default="cpu", help="running on cpu only!, default=False")
+    args = parser.parse_args()
+
+    # cfg
+    config_file = args.config  # change the path of the model config file
+    grounded_checkpoint = args.grounded_checkpoint  # change the path of the model
+    sam_checkpoint = args.sam_checkpoint
+    image_path = args.input_image
+    # text_prompt = args.text_prompt
+    output_dir = args.output_dir
+    box_threshold = args.box_threshold
+    text_threshold = args.box_threshold
+    device = args.device
+
+    # make dir
+    os.makedirs(output_dir, exist_ok=True)
+    # load image
+    image_pil, image = load_image(image_path)
+    # load model
+    model = load_model(config_file, grounded_checkpoint, device=device)
+
+    # visualize raw image
+    image_pil.save(os.path.join(output_dir, "raw_image.jpg"))
+
+    processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+    captioner = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large", torch_dtype=torch.float16)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    captioner.to(device)
+
+    inputs = processor(image_pil, return_tensors="pt").to(device, torch.float16)
+    
+    generated_ids = captioner.generate(**inputs, max_new_tokens=20)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+    print(generated_text)
+
+    # chat_prompt_template = ChatPromptTemplate.from_messages([human_message_prompt])
+    # chat = ChatOpenAI(temperature=0.9)
+    # chain = LLMChain(llm=chat, prompt=chat_prompt_template)
+    # print(chain.run(generated_text))
+
+    # official example
+    graph = sng_parser.parse(generated_text)
+    pprint(graph)
+    sng_parser.tprint(graph)  # we provide a tabular visualization of the graph.
+
+    boxes_filt = []
+    pred_phrases = []
+
+    entities = graph["entities"]
+    for item in entities:
+        # run grounding dino model
+        cur_boxes_filt, cur_pred_phrases = get_grounding_output(
+            model, image, item['head'], box_threshold, text_threshold, device=device
+        )
+        boxes_filt.extend(cur_boxes_filt)
+        pred_phrases.extend(cur_pred_phrases)
+
+    # initialize SAM
+    sam = build_sam(checkpoint=sam_checkpoint)
+    sam.to(device=device)
+    predictor = SamPredictor(sam)
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    predictor.set_image(image)
+
+    boxes_filt = torch.stack(boxes_filt)
+    
+    size = image_pil.size
+    H, W = size[1], size[0]
+    for i in range(boxes_filt.size(0)):
+        boxes_filt[i] = boxes_filt[i] * torch.Tensor([W, H, W, H])
+        boxes_filt[i][:2] -= boxes_filt[i][2:] / 2
+        boxes_filt[i][2:] += boxes_filt[i][:2]
+
+    boxes_filt = boxes_filt.cpu()
+    transformed_boxes = predictor.transform.apply_boxes_torch(boxes_filt, image.shape[:2]).to(device)
+
+    masks, _, _ = predictor.predict_torch(
+        point_coords = None,
+        point_labels = None,
+        boxes = transformed_boxes,
+        multimask_output = False,
+    )
+    
+    # draw output image
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    for mask in masks:
+        show_mask(mask.cpu().numpy(), plt.gca(), random_color=True)
+    for box, label in zip(boxes_filt, pred_phrases):
+        show_box(box.numpy(), plt.gca(), label)
+
+    plt.axis('off')
+    plt.savefig(
+        os.path.join(output_dir, "grounded_sam_output.jpg"), 
+        bbox_inches="tight", dpi=300, pad_inches=0.0
+    )
+
+    save_mask_data(output_dir, masks, boxes_filt, pred_phrases)
+
